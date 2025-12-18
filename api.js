03d95fde4a224b2649a0b30db224ae3deef78208
savithaprofile <savitha.saviy@gmail.com>
2025-12-18 10:43:14 +0530

    api handling

--- api.js
+++ api.js
@@ -2,9 +2,21 @@
 // 2. response status and change to json
 // 3. using the json
 
-fetch("https://api.adviceslip.com/advice")
-.then(res => res.json())
-.then(data=>{
-    console.log("Quote",data.slip.advice);
-})
+//.catch() : catches the error. Only gets executed if there is an error
+//.finally() : gets executed no matter what, when the fetch is done, even if there is an error
 
+fetch("https://api.adviceslip.com/advice")
+.then(res => res.jon())
+.then(data=>{
+    console.log(data);
+    console.log("Advice:",data.slip.advice);
+})
+.catch(()=>{
+    console.log("Some Error Occured in fetch");
+})
+.finally(()=>{
+    console.log("the fetch is done");
+});
+
+
+//async and await
